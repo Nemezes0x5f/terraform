fdc9839ef7e70dc67bd889961fb911adfa4eb38a
Nemzes0x5f <rodrigo.mz.sousa@gmail.com>
2022-03-07 18:02:00 -0300

    Creating Security Group

--- main.tf
+++ main.tf
@@ -26,3 +26,18 @@ resource "aws_key_pair" "dev" {
   key_name   = "dev"
   public_key = file("dev.pub")
 }
+
+resource "aws_security_group" "ssh-access" {
+  name        = "ssh-access"
+  description = "ssh-access"
+  ingress {
+    description = "ssh-access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "ssh-access"
+  }
+}
