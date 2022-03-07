060b1bc658e9ce655e09b63dc9db019ec704e798
Nemzes0x5f <rodrigo.mz.sousa@gmail.com>
2022-03-06 21:40:18 -0300

    First Commit

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "dev" {
+  count         = 3
+  ami           = "ami-0892d3c7ee96c0bf7"
+  instance_type = "t2.micro"
+  key_name      = "dev"
+  tags = {
+    Name = "Dev Server ${count.index}"
+  }
+}
+
+resource "aws_key_pair" "dev" {
+  key_name   = "dev"
+  public_key = file("dev.pub")
+}
