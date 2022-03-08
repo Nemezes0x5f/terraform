61d59ed888b9e975f43ab39e5195901c63086cc2
Nemzes0x5f <rodrigo.mz.sousa@gmail.com>
2022-03-07 22:55:06 -0300

    Creating DynamoDB & SSH key in us-west-1, and finishing Class 4

--- main.tf
+++ main.tf
@@ -55,15 +55,24 @@ resource "aws_instance" "dev6" {
   provider      = aws.us-west-1
   ami           = "ami-01f87c43e618bf8f0"
   instance_type = "t2.micro"
-  key_name      = "dev"
+  key_name      = "dev-us-west-1"
   tags = {
     Name = "Dev Server 6"
   }
   vpc_security_group_ids = [aws_security_group.ssh-access-us-west-2.id]
+  depends_on = [
+    aws_dynamodb_table.dynamodb-homologacao
+  ]
 }
 
 resource "aws_key_pair" "dev" {
   key_name   = "dev"
+  public_key = file("dev.pub")
+}
+
+resource "aws_key_pair" "dev-us-west-1" {
+  provider   = aws.us-west-1
+  key_name   = "dev-us-west-1"
   public_key = file("dev.pub")
 }
 
@@ -76,3 +85,20 @@ resource "aws_s3_bucket" "dev-4-bucket" {
   }
 }
 
+resource "aws_dynamodb_table" "dynamodb-homologacao" {
+  provider     = aws.us-west-1
+  name         = "GameScores"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "UserId"
+  range_key    = "GameTitle"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  attribute {
+    name = "GameTitle"
+    type = "S"
+  }
+}
