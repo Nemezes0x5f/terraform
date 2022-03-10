1c8383ba025d2cfafb723367b3e0a13440e4c208
Nemzes0x5f <rodrigo.mz.sousa@gmail.com>
2022-03-09 21:29:18 -0300

    Removing resources

--- main.tf
+++ main.tf
@@ -28,18 +28,18 @@ resource "aws_instance" "dev" {
   vpc_security_group_ids = [aws_security_group.ssh-access.id]
 }
 
-resource "aws_instance" "dev4" {
-  ami           = "ami-0892d3c7ee96c0bf7"
-  instance_type = "t2.micro"
-  key_name      = "dev"
-  tags = {
-    Name = "Dev Server 4"
-  }
-  vpc_security_group_ids = [aws_security_group.ssh-access.id]
-  depends_on = [
-    aws_s3_bucket.dev-4-bucket
-  ]
-}
+# resource "aws_instance" "dev4" {
+#   ami           = "ami-0892d3c7ee96c0bf7"
+#   instance_type = "t2.micro"
+#   key_name      = "dev"
+#   tags = {
+#     Name = "Dev Server 4"
+#   }
+#   vpc_security_group_ids = [aws_security_group.ssh-access.id]
+#   depends_on = [
+#     aws_s3_bucket.dev-4-bucket
+#   ]
+# }
 
 resource "aws_instance" "dev5" {
   ami           = "ami-0892d3c7ee96c0bf7"
@@ -65,14 +65,14 @@ resource "aws_instance" "dev6" {
   ]
 }
 
-resource "aws_s3_bucket" "dev-4-bucket" {
-  bucket = "dev-4-bucket"
-  acl    = "private"
-  tags = {
-    Name        = "Dev 4 Bucket"
-    Environment = "Dev"
-  }
-}
+# resource "aws_s3_bucket" "dev-4-bucket" {
+#   bucket = "dev-4-bucket"
+#   acl    = "private"
+#   tags = {
+#     Name        = "Dev 4 Bucket"
+#     Environment = "Dev"
+#   }
+# }
 
 resource "aws_dynamodb_table" "dynamodb-homologacao" {
   provider     = aws.us-west-1
